83f631b87b88e0ae6e8930854b2d013f7fa0abe5
Chelsea Wilkinson <122856043+chelsea6502@users.noreply.github.com>
2024-12-14 14:43:10 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 curl https://raw.githubusercontent.com/chelsea6502/impermanent-setup/main/final/disko.nix -o /tmp/disko.nix
 
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/vda"'
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko.nix --arg device '"/dev/nvme0n1"'
 
 sudo nixos-generate-config --no-filesystems --root /mnt
 
